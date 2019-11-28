8964d118ad67b7a9bd8869fd2e024a36dd7ce76c
Lynn Nguyen <55603454+meloly4@users.noreply.github.com>
2019-11-27 21:38:56 -0600

    Add files via upload

---
+++ memeMaker/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace memeMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.save = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bottomText = new System.Windows.Forms.TextBox();
+            this.topText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.preview = new System.Windows.Forms.PictureBox();
+            this.topLabel = new System.Windows.Forms.Label();
+            this.bottomLabel = new System.Windows.Forms.Label();
+            this.open = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(1093, 621);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(117, 36);
+            this.save.TabIndex = 1;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bottomText);
+            this.groupBox1.Controls.Add(this.topText);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(40, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(582, 543);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Edit";
+            // 
+            // bottomText
+            // 
+            this.bottomText.Location = new System.Drawing.Point(26, 326);
+            this.bottomText.Multiline = true;
+            this.bottomText.Name = "bottomText";
+            this.bottomText.Size = new System.Drawing.Size(526, 187);
+            this.bottomText.TabIndex = 3;
+            this.bottomText.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // topText
+            // 
+            this.topText.Location = new System.Drawing.Point(26, 62);
+            this.topText.Multiline = true;
+            this.topText.Name = "topText";
+            this.topText.Size = new System.Drawing.Size(526, 185);
+            this.topText.TabIndex = 2;
+            this.topText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bottom Text";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Top Text";
+            // 
+            // preview
+            // 
+            this.preview.Location = new System.Drawing.Point(628, 41);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(582, 537);
+            this.preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.preview.TabIndex = 4;
+            this.preview.TabStop = false;
+            this.preview.Click += new System.EventHandler(this.preview_Click);
+            // 
+            // topLabel
+            // 
+            this.topLabel.AutoSize = true;
+            this.topLabel.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLabel.ForeColor = System.Drawing.Color.White;
+            this.topLabel.Location = new System.Drawing.Point(628, 103);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(87, 35);
+            this.topLabel.TabIndex = 5;
+            this.topLabel.Text = "label3";
+            // 
+            // bottomLabel
+            // 
+            this.bottomLabel.AutoSize = true;
+            this.bottomLabel.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomLabel.ForeColor = System.Drawing.Color.White;
+            this.bottomLabel.Location = new System.Drawing.Point(629, 519);
+            this.bottomLabel.Name = "bottomLabel";
+            this.bottomLabel.Size = new System.Drawing.Size(86, 35);
+            this.bottomLabel.TabIndex = 6;
+            this.bottomLabel.Text = "label4";
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(970, 621);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(117, 36);
+            this.open.TabIndex = 7;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 692);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.bottomLabel);
+            this.Controls.Add(this.topLabel);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.save);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Meme Maker by LMC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox bottomText;
+        private System.Windows.Forms.TextBox topText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox preview;
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Label bottomLabel;
+        private System.Windows.Forms.Button open;
+    }
+}
+
---
+++ memeMaker/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memeMaker
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openImage;
+        string imageFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            topLabel.Text = topText.Text;
+            topLabel.MaximumSize = new Size(preview.Width, 200);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            bottomLabel.Text = bottomText.Text;
+            bottomLabel.MaximumSize = new Size(preview.Width, 200);
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            openImage = new OpenFileDialog();
+            openImage.InitialDirectory = "c:\\";
+            openImage.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *jfif; *.png; *.gif; *.bmp";
+            openImage.FilterIndex = 2;
+            openImage.RestoreDirectory = true;
+
+            if (openImage.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    preview.Image = Image.FromFile(openImage.FileName);
+                    imageFile = openImage.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Yo I can't read this shit. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string firstText = topText.Text;
+            string secondText = bottomText.Text;
+            
+            Bitmap bitmap = (Bitmap)Image.FromFile(imageFile); //load the image file
+
+            RectangleF TopSize = new RectangleF(0, 10, bitmap.Width, 400);
+            RectangleF BottomSize = new RectangleF(0, bitmap.Height - 100, bitmap.Width, 400);
+
+            SaveFileDialog saveImage = new SaveFileDialog();
+            saveImage.FileName = "*";
+            saveImage.DefaultExt = "bmp";
+            saveImage.ValidateNames = true;
+            saveImage.Filter = "Bitmap Image (.bmp) |*.bmp| Gif Image (.gif) |*.gif| JPEG Image (.jpg) |*.jpeg| Png Image (.png) |*.png";
+
+            if (saveImage.ShowDialog() == DialogResult.OK)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Font memeFont = new Font("Impact", 24, FontStyle.Bold, GraphicsUnit.Point))
+                    {
+                        graphics.DrawString(firstText, memeFont, Brushes.White, TopSize);
+                        graphics.DrawString(secondText, memeFont, Brushes.White, BottomSize);
+                    }
+                }
+                bitmap.Save(saveImage.FileName); //save the image file
+            }
+        }
+
+        private void preview_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
